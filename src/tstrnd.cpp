54c48a6b3a57462997df945acd5324ec7e4f17c1
Marian Triebe <m.triebe@live.de>
2021-06-05 10:03:49 +0200

    Add default engine for testing prupose

--- src/tstrnd.cpp
+++ src/tstrnd.cpp
@@ -12,6 +12,7 @@ using namespace taski;
 int main() {
   detail::xorwow xor_engine;
   std::mt19937 mt_engine;
+  std::random_device df_engine;
   // Measure time
   auto tp1 = std::chrono::high_resolution_clock::now();
   for (int i = 0; i < ITERATIONS; ++i) {
@@ -22,9 +23,14 @@ int main() {
     static_cast<void>(mt_engine());
   }
   auto tp3 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < ITERATIONS; ++i) {
+    static_cast<void>(df_engine());
+  }
+  auto tp4 = std::chrono::high_resolution_clock::now();
 
   std::uniform_int_distribution<size_t> dist1(0, 100);
   std::uniform_int_distribution<size_t> dist2(0, 100);
+  std::uniform_int_distribution<size_t> dist3(0, 100);
   // Check distribution, expect near 50
   double xor_mean = 0.0;
   for (int i = 0; i < ITERATIONS; ++i) {
@@ -36,11 +42,20 @@ int main() {
     mt_mean += dist2(mt_engine);
   }
   mt_mean /= ITERATIONS;
+  double df_mean = 0.0;
+  for (int i = 0; i < ITERATIONS; ++i) {
+    df_mean += dist3(df_engine);
+  }
+  df_mean /= ITERATIONS;
 
   std::cout << "xorwow:   " << std::chrono::duration_cast<std::chrono::microseconds>(tp2 - tp1).count() << "µs\n";
-  std::cout << "mt19937:  " << std::chrono::duration_cast<std::chrono::microseconds>(tp3 - tp2).count() << "µs" << std::endl;
+  std::cout << "mt19937:  " << std::chrono::duration_cast<std::chrono::microseconds>(tp3 - tp2).count() << "µs\n";
+  std::cout << "default:  " << std::chrono::duration_cast<std::chrono::microseconds>(tp4 - tp3).count() << "µs" << std::endl;
 
   std::cout << "xor_mean: " << xor_mean << "\n";
-  std::cout << "mt_mean:  " << mt_mean << std::endl;
+  std::cout << "mt_mean:  " << mt_mean << "\n";
+  std::cout << "df_mean: " << df_mean << std::endl;
+
+  std::cout << xor_engine() << std::endl;
 }
 
